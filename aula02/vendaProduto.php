1d3f338a693ee1c36f0a8a29ba83e3c4bb9153bd
Bruno Balbinot <bruno62254736@alunos.sc.senac.br>
2026-06-17 22:05:16 -0300

    Repositorio PHP

--- aula02/vendaProduto.php
+++ aula02/vendaProduto.php
@@ -1,25 +1,5 @@
 <?php
 
-define("LOJA", "Tech Point");
-
-// 2. Variáveis de dados
-$produto = "Mouse Gamer";
-$preco = 89.90; // Float
-$quantidade = 2; // Int
-
-// 3. Operação Aritmética
-$total = $preco * $quantidade;
-
-// 4. Saída de dados (Usando aspas duplas para interpretar as variáveis)
-echo "--- " . LOJA . " ---<br>";
-echo "Produto: $produto <br>";
-echo "Quantidade: $quantidade <br>";
-echo "Valor Total: R$ $total";
-
-
-
-/*
-
 define("GAMES", "League of Legends");
 
 $skins = "Skins";
@@ -31,8 +11,6 @@ $totalVendido = ($valor * $quantidade);
 echo "A loja se chama ".GAMES."
 o nome do produto  é $skins 
 e o valor total é $totalVendido";
-
-*/
 
 
 ?>
